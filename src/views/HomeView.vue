262737125905225d44ed6d75ac288162f24810fc
Dan Carlier <dan.carlier@gmail.com>
2022-08-31 16:38:08 +1000

    Added counter title input field, and moved count and title to inside a reactive object

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,74 +1,45 @@
 <template>
   <div class="home">
   
+    <h2>{{ appTitle }}</h2>
+
+    <h3>{{ counterData.title }}:</h3>
+
     <div>
       <button @click="decreaseCounter" class="btn">-</button>
-      <span class="counter">{{ counter }}</span>
+      <span class="counter">{{ counterData.count }}</span>
       <button @click="increaseCounter" class="btn">+</button>
+    </div>
+
+    <div class="edit">
+      <h4>Edit counter title:</h4>
+      <input v-model="counterData.title" type="text">
     </div>
 
   </div>
 </template>
 
 <script setup>
-  import { ref } from 'vue'
-  
-  const counter = ref(0)
+  import { reactive } from 'vue'
+
+  const appTitle = 'My Counter App'
+
+  // const counter = ref(0)
+  // const counterTitle = ref('My Counter')
+
+  const counterData = reactive({
+    count: 0,
+    title: 'My Counter'
+  })
 
   const increaseCounter = () => {
-    counter.value++
+    counterData.count++
   }
 
   const decreaseCounter = () => {
-    counter.value--
+    counterData.count--
   }
 </script>
-
-<!--
-<script>
-import { ref } from 'vue'
-
-export default {
-  setup() {
-    const counter = ref(0)
-
-    const increaseCounter = () => {
-      counter.value++
-    }
-
-    const decreaseCounter = () => {
-      counter.value--
-    }
-
-    return {
-      counter,
-      decreaseCounter,
-      increaseCounter
-    }
-  }
-}
-</script>
--->
-
-<!--
-<script>
-export default {
-  data() {
-    return {
-      counter: 0
-    }
-  },
-  methods: {
-    increaseCounter() {
-      this.counter++
-    },
-    decreaseCounter() {
-      this.counter--
-    }
-  }
-}
-</script>
--->
 
 <style>
 .home {
@@ -78,5 +49,8 @@ export default {
 .btn, .counter {
   font-size: 40px;
   margin: 10px;
+}
+.edit {
+  margin-top: 60px;
 }
 </style>
