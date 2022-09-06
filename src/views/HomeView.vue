7d26255279e1308c31dd6286e0f8ec113a00ec4e
Dan Carlier <dan.carlier@gmail.com>
2022-09-06 11:52:17 +1000

    Loop over and show posts, created ModalsView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
   
-    <h2>{{ appTitle }}</h2>
+    <h2 ref="appTitleRef">{{ appTitle }}</h2>
 
     <h3>{{ counterData.title }}:</h3>
 
@@ -28,7 +28,7 @@
     imports
   */
 
-  import { reactive, computed, watch, onMounted } from 'vue'
+  import { ref, reactive, computed, watch, onMounted, nextTick } from 'vue'
   import { vAutofocus } from '@/directives/vAutofocus'
   
   /*
@@ -37,8 +37,10 @@
 
   const appTitle = 'My Counter App'
 
+  const appTitleRef = ref(null)
+
   onMounted(() => {
-    console.log('Do stuff related to app title')
+    console.log(`The app title is ${ appTitleRef.value.offsetWidth } px wide!`)
   })
 
   /*
@@ -63,9 +65,11 @@
     return 'odd'
   })
 
-  const increaseCounter = (amount, e) => {
+  const increaseCounter = async (amount, e) => {
     console.log('e', e)
     counterData.count += amount
+    await nextTick()
+    console.log('do something when counter has updated in the dom')
   }
 
   const decreaseCounter = () => {
